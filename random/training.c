a919178c42fa47472284fb65d3919502752ff675
fornesarturo <fornesarturo@gmail.com>
2017-08-10 19:47:15 -0500

    pointer fun

--- random/training.c
+++ random/training.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, char ** argv) {
 	// Variable declaration.
-    char *string = malloc(sizeof(char));
+    char *string = (char *) malloc(sizeof(char));
 
 	// Body.
 	*string = 'a';
     printf("Size of string is: %lu \n", sizeof(string));
 	printf("The value of string, which is *string is: %c \n", *string);
+
+    // Let's see the size of the variables.
+    printf("Char: %lu\nInt: %lu\nFloat: %lu\nDouble: %lu\n", sizeof(char), sizeof(int), sizeof(float), sizeof(double));
 
 	// Post-Execution.
 	free(string);
