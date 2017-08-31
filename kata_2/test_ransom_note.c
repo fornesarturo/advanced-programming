33aa33ef14355b91172077189b21df50804c9cda
fornesarturo <fornesarturo@gmail.com>
2017-08-31 16:54:50 -0500

    more cases in test_ransom_note

--- kata_2/test_ransom_note.c
+++ kata_2/test_ransom_note.c
@@ -15,23 +15,70 @@
 
 int tests_run = 0;
 
+static char * test_construct_0 () {
+    char * ransom_note_test = "help me pls";
+    char * magazine_test = "";
+    int result = can_construct(ransom_note_test, magazine_test);
+    mu_assert("Error, empty magazine should be false.", result == 0);
+    return 0;
+}
+
 static char * test_construct_1 () {
-    char  *ransom_note_test = "hola amigoso";
-    char * magazine_test = "alohsogmio";
+    char * ransom_note_test_2 = "";
+    char * magazine_test_2 = "aabcde";
+    int result_2 = can_construct(ransom_note_test_2, magazine_test_2);
+    mu_assert("Error, empty magazine should be false.", result_2 == 1);
+    return 0;
+}
+
+static char * test_construct_2 () {
+    char  * ransom_note_test = "hola amigoso";
+    char * magazine_test = "alohsogmioa";
     int result = can_construct(ransom_note_test, magazine_test);
     mu_assert("Error, this result should be true.", result == 1);
     return 0;
 }
 
-static char * test_construct_0 () {
-    char * ransom_note_test = "help me pls";
-    char * magazine_test = "";
+static char * test_construct_3 () {
+    char  * ransom_note_test = "hablar contigo";
+    char * magazine_test = "saludosamigo";
     int result = can_construct(ransom_note_test, magazine_test);
     mu_assert("Error, this result should be false.", result == 0);
+    return 0;
+}
+
+static char * test_construct_4 () {
+    char  * ransom_note_test = "hablar contigo";
+    char * magazine_test = "a";
+    int result = can_construct(ransom_note_test, magazine_test);
+    mu_assert("Error, this magazine can't form the ransom note", result == 0);
+    return 0;
+}
+
+static char * test_construct_5 () {
+    char  * ransom_note_test = "hola";
+    char * magazine_test = "alo";
+    int result = can_construct(ransom_note_test, magazine_test);
+    mu_assert("Error, this magazine can't form the ransom note", result == 0);
+    return 0;
+}
+
+static char * test_construct_6 () {
+    char  * ransom_note_test = "sdjbfdkfglsk asdfdsdfgdgddfvcbd";
+    char * magazine_test = "sdjbfdkfglsk";
+    int result = can_construct(ransom_note_test, magazine_test);
+    mu_assert("Error, this magazine can't form the ransom note", result == 0);
+    return 0;
 }
 
 static char * all_tests () {
-    mu_run_test(test_construct_1);
+	mu_run_test(test_construct_0);
+	mu_run_test(test_construct_1);
+	mu_run_test(test_construct_2);
+	mu_run_test(test_construct_3);
+	mu_run_test(test_construct_4);
+	mu_run_test(test_construct_5);
+	mu_run_test(test_construct_6);
     return 0;
 }
 
