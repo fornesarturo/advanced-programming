966015bfb18b845ea225a23e22439fcc991d99ae
fornesarturo <fornesarturo@gmail.com>
2017-08-31 21:12:15 -0500

    Mini-Project 1
    Problem to solve "Can I Place Flowers"

---
+++ mini-project_1/can_place_flowers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "can_place_flowers.h"
+
+/** can_place_flower
+  *
+  * Given a flowerbed (represented as an array containing 0 and 1, where 0
+  * means empty and 1 means not empty), and a number n, return if n new flowers
+  * can be planted in it without violating the no-adjacent-flowers rule.
+  * Notes:
+  *     The input array won't violate no-adjacent-flowers rule.
+  *     The input array size is in the range of [1, 20000].
+  *     n is a non-negative integer which won't exceed the input array size.
+  * @params:
+  *     flowerbed: an array containing 0 and 1 representing the flowerbed.
+  *     flowerbed_size: the size of the array flowerbed.
+  *     n: the number of flowers to be placed.
+  * @return:
+  *     int: 1 | 0
+*/
+int can_place_flowers(int * flowerbed, int flowerbed_size, int n) {
+    
+}
