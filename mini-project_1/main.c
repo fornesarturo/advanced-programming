d7245e8e58dc0c89c5c3143e59bcc0583549d143
fornesarturo <fornesarturo@gmail.com>
2017-09-06 23:04:38 -0500

    Main implemented
    Implemented into main.c a graphic (in terminal) way to see how the
    solution to Can Place Flowers works.

--- mini-project_1/main.c
+++ mini-project_1/main.c
@@ -2,11 +2,86 @@
 #include <stdio.h>
 #include "can_place_flowers.h"
 
-/* Add function signatures here */
-
 int main(int argc, char **argv) {
-    /* Start your code here */
-    printf("Hello from main\n");
-    int array[] = {1,2,3};
+    int flowerbed[] = {1,0,1,0,0,0,1,0,0};
+    int flowerbed_size = 9;
+    int n = 3, i, f_counter;
+    int result = can_place_flowers(flowerbed, flowerbed_size, n);
+
+    // The following monster of code is to print a 'report' of the given case.
+    // It includes where the flowers were placed or could have been placed and
+    // whether n flowers where able to be planted.
+    printf("\n\n\nResult is %d\n", result);
+    if (result) {
+        f_counter = n;
+        printf("You're able to place %d flowers in the flowerbed: \n", n);
+        for (i = 0; i < flowerbed_size; i++) {
+            printf(" %d ", flowerbed[i]);
+        }
+        printf("\n\nThese are the following places in which a flower was placed, denoted by F:\n");
+        for(i = 0; i < flowerbed_size; i++) {
+            if( i == 0 ) {
+                if(flowerbed[i + 1] == 0 && flowerbed[i] == 0 && f_counter > 0) {
+                    printf(" F ");
+                    f_counter--;
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else if( i == flowerbed_size - 1 ) {
+                if(flowerbed[i - 1] == 0 && f_counter > 0) {
+                    printf(" F ");
+                    f_counter--;
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else if(flowerbed[i] == 0) {
+                if(flowerbed[i - 1] == 0 && flowerbed[i+1] == 0 && f_counter > 0) {
+                    printf(" F ");
+                    f_counter--;
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else {
+                printf(" %d ", flowerbed[i]);
+            }
+        }
+
+        printf("\n\n");
+    }
+    else {
+        printf("You're not able to place %d flowers in the flowerbed", n);
+        printf("\n\nThese are the following places in which you can place a flower, denoted by F:\n");
+        for(i = 0; i < flowerbed_size; i++) {
+            if( i == 0 ) {
+                if(flowerbed[i + 1] == 0 && flowerbed[i] == 0) {
+                    printf(" F ");
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else if( i == flowerbed_size - 1 ) {
+                if(flowerbed[i - 1] == 0) {
+                    printf(" F ");
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else if(flowerbed[i] == 0) {
+                if(flowerbed[i - 1] == 0 && flowerbed[i+1] == 0) {
+                    printf(" F ");
+                } else {
+                    printf(" %d ", flowerbed[i]);
+                }
+            }
+            else {
+                printf(" %d ", flowerbed[i]);
+            }
+        }
+
+        printf("\n\n");
+    }
     return 0;
 }
