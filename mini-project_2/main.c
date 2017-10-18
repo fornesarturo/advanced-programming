53211e14ff1b8a3fae5188740bc842370381c850
fornesarturo <fornesarturo@gmail.com>
2017-10-18 12:04:27 -0500

    Reverse String: main

--- mini-project_2/main.c
+++ mini-project_2/main.c
@@ -1,11 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "reverse_string.h"
 
-/* Add function signatures here */
+#define print_debug(msg) do { \
+	char * value = getenv("REVERSE_DEBUG");\
+	if (value != NULL && !strcmp(value, "t")) printf("%s", msg);\
+} while(0);
+
 
 int main(int argc, char **argv) {
-    /* Start your code here */
-    printf("Hello from main\n");
-    return 0;
+	
+	char string[40];
+	char message[40];
+
+	printf("Enter the string you want reversed: \n");
+	scanf("%s", string);
+	
+	sprintf(message, "%s\n", string);
+	print_debug(message);
+
+	char * reverse = reverse_string(string);
+
+	if (reverse != NULL) {
+		printf("Reversed %s\n", reverse);	
+	}
+	
+	return 0;
 }
