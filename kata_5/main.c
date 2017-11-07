6c7d6631a50380be61a650847a5e98c5994e89ff
fornesarturo <fornesarturo@gmail.com>
2017-11-07 15:48:07 -0600

    Kata 5: main

--- kata_5/main.c
+++ kata_5/main.c
@@ -5,7 +5,13 @@
 /* Add function signatures here */
 
 int main(int argc, char **argv) {
-    /* Start your code here */
-    printf("Hello from main\n");
+    int n;
+    printf("Value of n: "); scanf("%d", &n);
+    int * size = (int *) malloc(sizeof(int));
+    char ** result = fizzbuzz(n, size);
+    int i = 0;
+    for (i = 0; i < *size; i++) {
+        printf("%s\n", result[i]);
+    }
     return 0;
 }
